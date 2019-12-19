16f84266b828ead1e70e5f97672b00c924fc19b4
jyl <jylhyl163.com>
2019-12-19 09:47:08 +0800

    first commit123tijiao

---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,21 @@
+package controller;
+/*
+ * 基于SSM框架的第一个控制器
+ * 1.添加@Controller
+ * 2.方法添加映射路径
+ * @author Jiayulin
+ * @since jdk
+ * */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	@RequestMapping("hi")
+	@ResponseBody
+    public String abc() {
+		return "123";
+	}
+}
